4888a488f0372dffafb132fb462a91e6ea5e981b
shadow <lightshadow416@gmail.com>
2023-12-05 23:59:19 +0100

    remvoing ul coin structor to test something

--- components/Coin/Coin.jsx
+++ components/Coin/Coin.jsx
@@ -34,21 +34,32 @@ export default function Coin({
         }}
         style={{ gridTemplateColumns: "15% 1fr 16% 16% 15% 15%" }}
         className="notFirstUl cursor-pointer px-[1.5rem] items-center grid  my-[1.7rem] py-[1rem] bg-gray-900 shadow-xl"
-      >
-        {/* <Link id="sb" href="/overview" clas></Link> */}
-        <li>{rank}</li>
-        <li className="flex items-center">
-          <img src={img} alt="coin" className="h-[3rem] mr-[1rem]" />
-          <span className="text-[1.2rem] pr-3">{name}</span>
-        </li>
-        {ARR_OF_COIN_DETAILS.map((e, i) => {
-          return (
-            <>
-              <li key={i}>{e}</li>
-            </>
-          );
-        })}
-      </ul>
+      ></ul>
     </>
   );
 }
+
+{
+  /* <ul
+        onClick={() => {
+          dispatch(addUuid(uuid));
+          router.push("/overview");
+        }}
+        style={{ gridTemplateColumns: "15% 1fr 16% 16% 15% 15%" }}
+        className="notFirstUl cursor-pointer px-[1.5rem] items-center grid  my-[1.7rem] py-[1rem] bg-gray-900 shadow-xl"
+      >
+        {/* <Link id="sb" href="/overview" clas></Link> */
+}
+//   <li>{rank}</li>
+//   <li className="flex items-center">
+//     <img src={img} alt="coin" className="h-[3rem] mr-[1rem]" />
+//     <span className="text-[1.2rem] pr-3">{name}</span>
+//   </li>
+//   {ARR_OF_COIN_DETAILS.map((e, i) => {
+//     return (
+//       <>
+//         <li key={i}>{e}</li>
+//       </>
+//     );
+//   })}
+// </ul> */}
